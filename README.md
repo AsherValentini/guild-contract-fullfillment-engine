96efd8b6199b1f1f2f1a4cafa877d47988506787
AsherValentini <ashermvalentini@gmail.com>
2025-08-12 16:46:00 +0200

    [Submission]

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # guild-contract-fullfillment-engine
 Mock DSA Test 5
+
+Context
+
+You’ve been hired as a backend engineer by the Craftwright's Guild, 
+a decentralized medieval supply chain where blacksmiths, alchemists, 
+and jewelers post contracts to request specific items in exchange for payment.
+
+Each contract lists:
+
+    The final item the customer wants (e.g., IronSword)
+    A list of recipes for crafting items (e.g., to make an IronSword, you might need IronIngot and WoodHandle)
+    A current inventory of raw materials the player has
+
+A contract must be fulfilled in full. 
+If you can't produce the requested item, you return a ContractFailure object 
+showing which sub-item(s) were missing or blocked. 
+If you can fulfill it, you must deduct all used materials from the inventory and return success.
+
+Some recipes are nested (e.g., to make a WoodHandle, you first need Plank, which is made from WoodLog).
---
+++ src/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+#include <unordered_map>
+#include <algorithm>
+
+using Item = std::string;
+
+struct Recipe {
+    Item output;
+    std::vector<Item> inputs;
+};
+
+struct ContractFailure {
+    Item blockedOn;
+};
+
+class CraftwrightEngine {
+public:
+    CraftwrightEngine(std::vector<Recipe> knownRecipes, std::unordered_map<Item, int> startingInventory)
+        : m_inventory(std::move(startingInventory)) {
+
+            for(auto recipe:knownRecipes) {
+                m_recipes[recipe.output] = recipe.inputs;
+            }
+        }
+
+    // You must implement this
+    std::optional<ContractFailure> fulfillContract(const Item& requestedItem) {
+
+        std::unordered_map<Item, int> tempInventory = m_inventory;
+
+        auto failedAt = tryFillItem(requestedItem, tempInventory);
+
+        if(failedAt.has_value()) {
+            ContractFailure item{failedAt.value()};
+            return item;
+        }
+        m_inventory = tempInventory;
+        return std::nullopt;
+
+
+    }
+
+    // Debugging helpers
+    void printInventory() const;
+
+private:
+    std::unordered_map<Item, int> m_inventory;
+    std::unordered_map<Item, std::vector<Item>> m_recipes;
+
+    std::optional<Item> tryFillItem (const Item& item, std::unordered_map<Item, int>& inventory) {
+
+        auto it = inventory.find(item);
+        if(it != inventory.end() && it->second > 0) {
+            it->second--;
+            return std::nullopt;
+        }
+
+        auto recipeIt = m_recipes.find(item);
+        if(recipeIt == m_recipes.end()) {
+            return item;
+        }
+
+        // try fill items that make this item?
+        for(auto& subItem:recipeIt->second) {
+            auto op = tryFillItem(subItem, inventory);
+            if( op.has_value()) {
+                return op.value();
+            }
+
+        }
+
+        return std::nullopt;
+    }
+
+};
+
+
+int main() {
+	std::vector<Recipe> recipes = {
+        {"IronSword", {"IronIngot", "WoodHandle"}},
+        {"IronIngot", {"IronOre"}},
+        {"WoodHandle", {"Plank"}},
+        {"Plank", {"WoodLog"}}
+    };
+
+    std::unordered_map<Item, int> inventory = {
+        {"IronOre", 1},
+        {"WoodLog", 0}  // <-- Uh oh
+    };
+
+    CraftwrightEngine engine(recipes, inventory);
+
+    auto result = engine.fulfillContract("IronSword");
+
+    if (result.has_value()) {
+        std::cout << "Blocked on: " << result->blockedOn << "\n";
+    } else {
+        std::cout << "Contract completed!\n";
+    }
+
+    //engine.printInventory();
+
+	return 0;
+}
